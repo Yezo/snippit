8db171fea3d69bcc048ea7f168b5cf88b875b2c7
Kev <sulpeum@gmail.com>
2023-02-13 00:33:05 -0500

    updated css

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export const NavButton = () => {
   return (
-    <nav className="flex justify-center items-center flex-wrap gap-2 mb-5 ">
+    <nav className="flex justify-center items-center flex-wrap gap-2 mb-1 ">
       {navigationList.map(({ index, title, link }) => (
         <Link
           to={link}
-          className={`rounded-md p-2 min-w-[6rem] text-primary text-sm select-none grid place-items-center bg-secondary hover:bg-accent hover:text-text hover:tracking-wider tracking-normal focus:bg-accent focus:text-secondary`}
+          className={`inline-flex items-center justify-center rounded border-2 border-[#171515] bg-[#171515] px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-[#171515] focus:bg-primary focus:text-secondary active:opacity-75 min-w-[6.5rem] `}
           key={index}
         >
           {title}
@@ -16,3 +16,6 @@ export const NavButton = () => {
     </nav>
   );
 };
+// inline-flex items-center rounded border-2 border-[#171515] bg-[#171515] px-5 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-[#171515] focus:outline-none focus:ring active:opacity-75
+
+// rounded-md p-2 min-w-[6rem] text-primary text-sm select-none grid place-items-center bg-secondary hover:bg-accent hover:text-text hover:tracking-wider tracking-normal focus:bg-accent focus:text-secondary
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -9,14 +9,30 @@ export const Searchbar = ({ setSearch }: Props) => {
   };
 
   return (
-    <section className="grid place-items-center">
+    <section className="grid place-items-center mb-2">
       <form>
-        <input
+        <label
+          htmlFor="searchbar"
+          className="relative block overflow-hidden border-b border-secondary pt-3 focus-within:border-secondary"
+        >
+          <input
+            type="searchbar"
+            id="searchbar"
+            placeholder="Search something..."
+            className="peer h-7 w-full border-none bg-transparent p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm focus:placeholder-transparent italic px-1 py-1 tracking-tighter"
+            onChange={handleSearch}
+          />
+
+          {/* <span className="absolute left-0 top-2 -translate-y-1/2 hidden text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm tracking-wide peer-focus:top-2 peer-focus:text-xs peer-focus:flex">
+            Search
+          </span> */}
+        </label>
+        {/* <input
           type="text"
           className="bg-secondary rounded-lg text-primary text-sm px-4 py-3 mb-8 mt-2 focus:outline-none  ring-text placeholder:text-primary placeholder:text-sm placeholder:text-opacity-50 max-w-[20rem] "
           placeholder="Search something..."
           onChange={handleSearch}
-        ></input>
+        ></input> */}
       </form>
     </section>
   );
